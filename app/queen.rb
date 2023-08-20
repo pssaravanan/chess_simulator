890688b9b8259d36831cc7cfd6178fc2485d73b9
Saravanan <saravananselvamani@gmail.com>
2023-08-20 21:41:08 +0530

    refactor: change next_cell implementation

--- app/queen.rb
+++ app/queen.rb
@@ -1,56 +1,23 @@
 class Queen < ChessPiece
   def possible_next_moves
-    (horizontal_cells + vertical_cells + diagonal_cells).sort
+    horizontal_moves = next_moves_in_direction(1, 0) + next_moves_in_direction(-1, 0)
+    vertical_moves = next_moves_in_direction(0, 1) + next_moves_in_direction(0, -1)
+    diagonal_moves = next_moves_in_direction(1, 1) + next_moves_in_direction(-1, -1) + \
+                     next_moves_in_direction(1, -1) + next_moves_in_direction(-1, 1)
+    (horizontal_moves + vertical_moves + diagonal_moves).sort
   end
 
   private
 
-  def horizontal_cells
+  def next_moves_in_direction(col_dir, row_dir)
     next_moves = []
-    next_cell(0, 1) do |position|
-      next_moves << position
-    end
-    next_cell(0, -1) do |position|
-      next_moves << position
-    end
-    next_moves
-  end
-
-  def vertical_cells
-    next_moves = []
-    next_cell(1, 0) do |position|
-      next_moves << position
-    end
-    next_cell(-1, 0) do |position|
-      next_moves << position
-    end
-    next_moves
-  end
-
-  def diagonal_cells
-    next_moves = []
-    next_cell(1, 1) do |position|
-      next_moves << position
-    end
-    next_cell(-1, -1) do |position|
-      next_moves << position
-    end
-    next_cell(-1, 1) do |position|
-      next_moves << position
-    end
-    next_cell(1, -1) do |position|
-      next_moves << position
-    end
-    next_moves
-  end
-
-  def next_cell(col_dir, row_dir)
     next_col_idx = COLUMNS.index(column) + col_dir
     next_row_idx = ROWS.index(row) + row_dir
     while within_boundary?(next_col_idx, next_row_idx)
-      yield("#{COLUMNS[next_col_idx]}#{ROWS[next_row_idx]}")
+      next_moves << "#{COLUMNS[next_col_idx]}#{ROWS[next_row_idx]}"
       next_col_idx += col_dir
       next_row_idx += row_dir
     end
+    next_moves
   end
 end
