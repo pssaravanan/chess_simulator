d1d52eaec3af8008e22c1926672d570b91b811ee
Saravanan <saravananselvamani@gmail.com>
2023-08-20 11:50:26 +0530

    feat: chess piece initialize & position validation

--- app/chess_piece.rb
+++ app/chess_piece.rb
@@ -1,5 +1,28 @@
 class ChessPiece
+  attr_accessor :position
+
+  def initialize(position)
+    raise 'error: invalid_position' unless position_valid?(position)
+
+    @position = position
+  end
+
   def possible_next_moves
-    ['A1']
+    raise 'base class'
+  end
+
+  private
+
+  def position_valid?(position)
+    col, row = position.to_s.split('')
+    position.to_s.length == 2 && (valid_columns.include?(col) && valid_rows.include?(row))
+  end
+
+  def valid_columns
+    ('A'..'H').to_a
+  end
+
+  def valid_rows
+    ('1'..'8').to_a
   end
 end
--- specs/chess_piece_spec.rb
+++ specs/chess_piece_spec.rb
@@ -1,8 +1,28 @@
 require './specs/spec_helper'
 
 describe 'ChessPiece' do
-  it 'should do something' do
-    piece = ChessPiece.new
-    expect(piece.possible_next_moves).to eq(['A1'])
+  context '#initialize' do
+    it 'should initialize a chess piece with position' do
+      piece = ChessPiece.new('A1')
+      expect(piece.position).to eq('A1')
+    end
+
+    it 'should raise error if row is greater than chess board boundary' do
+      expect{
+        ChessPiece.new('I1')
+      }.to raise_error 'error: invalid_position'
+    end
+
+    it 'should raise error if col is greater than chess board boundary' do
+      expect{
+        ChessPiece.new('A9')
+      }.to raise_error 'error: invalid_position'
+    end
+
+    it 'should raise error if position string length greater than 2' do
+      expect{
+        ChessPiece.new('A10')
+      }.to raise_error 'error: invalid_position'
+    end
   end
 end
